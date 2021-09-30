5eaddf30ba01ba3b4b3280486f5c2c2ce60a8ae4
Joey Rivera <josephrivera5000@gmail.com>
2021-09-29 20:19:58 -0700

    Rendered expense data to ExpenseTable component

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -5,25 +5,29 @@ const ExpenseTable = ({users}) => {
 
   return (
     <>
-      <h2>Expense</h2>
+      <h2>Expenses</h2>
       <Table striped bordered hover variant="dark">
         <thead>
           <tr>
             <th>#</th>
             <th>First Name</th>
-            <th>Last Name</th>
-            <th>Total Expenses</th>
+            <th>Category</th>
+            <th>Description</th>
+            <th>Cost</th>
           </tr>
         </thead>
         <tbody>
           { users &&
-            Object.entries(users).map(([userId, user], index) =>
-              <tr key={userId}>
-                <td>{index + 1}</td>
-                <td>{user.firstName}</td>
-                <td>{user.lastName}</td>
-                <td>{user.totalExpenses}</td>
-              </tr>
+            Object.entries(users).map(([userId, user], index) => (
+              user.expenses.map(expense =>
+                <tr key={userId}>
+                  <td>{index + 1}</td>
+                  <td>{user.firstName + ' ' + user.lastName}</td>
+                  <td>{expense.category}</td>
+                  <td>{expense.description}</td>
+                  <td>{expense.cost}</td>
+                </tr>
+              ))
             )}
         </tbody>
       </Table>
